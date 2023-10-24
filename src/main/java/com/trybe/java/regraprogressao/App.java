ae47bdbff836fa2122797d96a1c1bfad48538f84
nataliaschmidt <nataliak.schmidt@gmail.com>
2023-10-24 14:42:18 -0300

    implementa o método para cadastrar as atividades avaliativas

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -1,5 +1,7 @@
 package com.trybe.java.regraprogressao;
 
+import java.util.Scanner;
+
 /**
  * App.
  */
@@ -9,5 +11,18 @@ public class App {
    * Metodo main.
    */
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Digite a quantidade de atividades para cadastrar:");
+
+    int registeredActivities = Integer.parseInt(scanner.nextLine());
+
+    for (int index = 0; index < registeredActivities; index++) {
+      System.out.println("Digite o nome da atividade " + (index + 1) + ":");
+      String activityName = scanner.nextLine();
+      System.out.println("Digite o peso da atividade " + (index + 1) + ":");
+      int activityNote = Integer.parseInt(scanner.nextLine());
+    }
+
+    scanner.close();
   }
 }
