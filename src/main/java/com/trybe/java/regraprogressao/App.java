da35568ca11ce5e64388013d53e1daabdfd9c308
nataliaschmidt <nataliak.schmidt@gmail.com>
2023-10-24 15:13:43 -0300

    Implementa no método o resultado final do aluno cadastrado

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -9,6 +9,8 @@ public class App {
 
   /**
    * Metodo main.
+   *
+   * @param args the input arguments
    */
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
@@ -16,6 +18,7 @@ public class App {
 
     int registeredActivities = Integer.parseInt(scanner.nextLine());
     int sumWeightActivities = 0;
+    double totalGrade = 0;
 
     for (int index = 0; index < registeredActivities; index++) {
       System.out.println("Digite o nome da atividade " + (index + 1) + ":");
@@ -28,11 +31,24 @@ public class App {
       System.out.println("Digite a nota obtida para " + activityName + ":");
       int activityNote = Integer.parseInt(scanner.nextLine());
 
-
+      totalGrade += activityNote * activityWeight;
     }
     scanner.close();
     if (sumWeightActivities != 100) {
       System.out.println("A soma dos pesos é diferente de 100!");
+    }
+
+    double finalGrade = totalGrade / 100;
+
+    if (finalGrade >= 85) {
+      System.out.println("Parabéns! Você alcançou "
+          + finalGrade
+          + "%! E temos o prazer de informar que você obteve aprovação! ");
+    } else {
+      System.out.println(
+          "Lamentamos informar que, com base na sua pontuação alcançada neste período, "
+              + finalGrade
+              + "%, você não atingiu a pontuação mínima necessária para sua aprovação.");
     }
   }
 }
